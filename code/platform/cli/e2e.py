52be195e1f4b6049c3d9680f0749b191a23956fa
Daniel R. Torres Ruiz <danielr.torresruiz@gmail.com>
2020-03-07 00:27:53 +0100

    [MODIFY] Platform CLI can read from register topic. - Next: Sync device and platform.

--- code/platform/cli/e2e.py
+++ code/platform/cli/e2e.py
@@ -1,29 +1,45 @@
+import paho.mqtt.client as mqtt
+import threading
 import click
+import time
 
-@click.group()
-def cli():
-  pass
+KEY_ID = "platform"
+KEY_SECRET = "platform-MUII"
+
+def on_connect(client, userdata, flags, rc):
+
+  print( "Platform is ready to work." )
+  print( "Connected with result code " + str( rc ) )
+  client.subscribe( "register" )
+
+def on_message( client, userdata, msg ):
+  # TODO: Separate topics.
+  print( msg.topic + " " + str( msg.payload ))
 
 @click.command()
-@click.option('-g', '--greeting', 'greeting', required=True, type=str, show_default=True, default="Hi", help="The way you greet")
-@click.option('-n', '--name', required=True, type=str, show_default=True, default="Siri", prompt="Your name")
-def hello(greeting, name):
-  """This script prints hello NAME."""
-  click.echo("%s, %s!" % (greeting, name))
+@click.option( '-s', '--server', 'server', required=True, type=str, show_default=True, default='broker.shiftr.io', help="The MQTT Server to send keys." )
+@click.option( '-p', '--port', 'port', required=True, type=int, show_default=True, default=1883, help="Port of theMQTT Server to send keys." )
+def connect( server, port ):
 
-@click.command()
-@click.option('-t', '--test', is_flag=True)
-def test(test):
-  """TEST!"""
-  if ( test ):
-    click.echo("This is also a test!")
-  else:
-    click.echo("This is a test!")
+  client = mqtt.Client()
+  client.on_connect = on_connect
+  client.on_message = on_message
+  client.username_pw_set( KEY_ID, KEY_SECRET )
+  client.connect( server, port, 60 )
 
+  client.loop_start()
+  
+  while True:    
+      # TODO: Reset keys after a while for each device
+      # client.publish( "KEYS", "HEY, I'M A KEY", 2 ) # TODO: Publish in different topics
+      time.sleep(1)
+
+# TODO: SEPARATE TASKS in commands
+# - Register new device
+# - List devices
+# - Remove devices
+# - Select and Read from an specific topic / device
+# - Run web platform
 
 if __name__ == '__main__':
-  
-  cli.add_command(hello)
-  cli.add_command(test)
-
-  cli()
+  connect()

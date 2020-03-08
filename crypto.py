fa26affc9592514086673e677428ca2b8118071d
Vadim Miron <mvadim88@gmail.com>
2020-03-08 17:45:55 +0100

    Implemented Simple Key Fernet and multy Key Fernet

--- crypto.py
+++ crypto.py
@@ -4,43 +4,59 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import dh
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 
+import sys
+import binascii
+import base64
+
+# Fernet is a symmetric encryption method which makes sure that the
+# message encrypted cannot be manipulated/read without the key. It 
+# uses URL safe encoding for the keys. Fernet uses 128-bit AES in 
+# CBC mode and PKCS7 padding, with HMAC using SHA256 for 
+# authentication. The IV is created from os.random(). This page 
+# decodes the token.
+
+
 # Fernet (symmetric encryption) with key rotation
-def fernetGenKeys():
+def multiKeysFernetGenKeys(keysNumber):
+    keys = []
+    for i in range(keysNumber):
+        #generate fernet keys for encrytp and decrypt data
+        keys.append(Fernet(Fernet.generate_key()))
+
+    f = MultiFernet(keys)
+    return f
+
+def simpleKeyFernetGenKey():
     #generate fernet keys for encrytp and decrypt data
-    key1 = Fernet(Fernet.generate_key()) 
-    key2 = Fernet(Fernet.generate_key())
-    key3 = Fernet(Fernet.generate_key())
-    
-    f = MultiFernet([key1, key2, key3])
+    f=Fernet(Fernet.generate_key())
     return f
 
 def fernetEncrypt(f, message):
     # encrypt data
     token = f.encrypt(message)
-    print("Encripted data:", token)
+    fernetPrint(token)
     return token
 
 def fernetDecrypt(f, token):
     m = f.decrypt(token)
+    print ("Decripted msg:",m.decode("utf-8"))
     return m
 
 # rotate token password
 def fernetKeyRotation(f, token):
     rotated = f.rotate(token)
+    fernetPrint(rotated)
     return rotated
 
-
-
-m = b"Hola"
-f = fernetGenKeys()
-t = fernetEncrypt(f, m)
-print("token:", t)
-print("decryptedMsg", fernetDecrypt(f,t))
-t = fernetKeyRotation(f,t)
-print("token:", t)
-fernetDecrypt(f, t)
-
-
+def fernetPrint(token):
+    cipher = binascii.hexlify(bytearray(token))
+    print ("\nFernet:\t\t",cipher.decode("utf-8"))
+    print ("Version:\t",cipher[0:2].decode("utf-8"))
+    print ("Time stamp:\t",cipher[2:18].decode("utf-8"))
+    #IV = initialization vector
+    print ("IV(Salt):\t",cipher[18:50].decode("utf-8"))
+    print ("Cypher:\t\t",cipher[50:-64].decode("utf-8"))
+    print ("HMAC:\t\t",cipher[-64:].decode("utf-8"))
 
 # Diffie Hellman 
 def non_ephemeral_DH():
@@ -70,7 +86,22 @@ def non_ephemeral_DH():
     server_shared_key = server_private_key.exchange(device_private_key.public_key())
     print("server_shared_key",server_shared_key)
 
-non_ephemeral_DH()
+# Diffie Hellman
+#non_ephemeral_DH()
+
+# Fernet test
+print("test encriptacion multi clave ")
+m = b"t"
+f = multiKeysFernetGenKeys(4)
+t = fernetEncrypt(f, m)
+t = fernetKeyRotation(f,t)
+d = fernetDecrypt(f, t)
+
+print("test encriptacion clave simple")
+m = b"test encriptacion clave simple"
+f = simpleKeyFernetGenKey()
+t = fernetEncrypt(f, m)
+d= fernetDecrypt(f, t)
 
 
 
